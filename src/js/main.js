2d563fc4689e1446c632384f1c8002e120392eaa
kanatbek04 <kanatbek.jenishbekov.04@gmail.com>
2023-12-09 19:27:15 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    $('ul.benefits__link').on('click', 'li:not(.active)', function() {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.benefits__row').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
+});
+var swiper = new Swiper(".team__swiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".team__next",
+        prevEl: ".team__prev",
+    },
+    autoplay:{
+        delay:2000
+    },
+    loop:true
+});
